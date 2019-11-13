e58969150da9eff861d6284d484f280bfbd3cfd4
TinyKitten <catwalk8205@gmail.com>
2019-11-13 18:03:02 +0900

    Firebaseトークンの隠蔽

--- nuxt.config.js
+++ nuxt.config.js
@@ -135,6 +135,7 @@ export default {
       }
     }
   },
+  buildModules: [['@nuxtjs/dotenv', { systemvars: true }]],
   manifest: {
     name: meta.title,
     short_name: meta.title,
@@ -143,5 +144,14 @@ export default {
     background_color: '#fff',
     description: meta.description,
     lang: 'ja'
+  },
+  env: {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    DATABASE_URL: process.env.DATABASE_URL,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+    APP_ID: process.env.APP_ID
   }
 }
--- vendor/firebase.js
+++ vendor/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBNO8zp5APTgz82lbhqXmBm-o5Nz3RYhJ0',
-  authDomain: 'webportfolio-4c591.firebaseapp.com',
-  databaseURL: 'https://webportfolio-4c591.firebaseio.com',
-  projectId: 'webportfolio-4c591',
-  storageBucket: 'webportfolio-4c591.appspot.com',
-  messagingSenderId: '971929767429',
-  appId: '1:971929767429:web:bf3bcf0d0c04b5b9'
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 }
 
 if (!firebase.apps.length) {
