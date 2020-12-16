9bbc1cade242e75df55000a67b354776af64afa8
TinyKitten <ts@tinykitten.me>
2020-12-17 03:29:20 +0900

    OGP

--- src/pages/works/trainlcd/index.tsx
+++ src/pages/works/trainlcd/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import TrainLCDFirstSection from './FirstSection';
 import TrainLCDConceptSection from './ConceptSection';
 import TrainLCDTechnologySection from './TechnologySection';
 import TrainLCDAccessSection from './AccessSection';
 import TrainLCDDesignSection from './DesignSection';
+import TrainLCDIMG from '../../../assets/works/trainlcd.png';
 
 const WorksTrainLCDPage: React.FC = () => {
   return (
     <>
+      <Helmet>
+        <title>TrainLCD</title>
+        <meta name="description" content="電車のLCDをスマホで" />
+        <meta property="og:site_name" content="TinyKitten" />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:url"
+          content={`${process.env.PUBLIC_URL}/works/trainlcd`}
+        />
+        <meta property="og:title" content="TrainLCD" />
+        <meta property="og:description" content="電車のLCDをスマホで" />
+        <meta property="og:image" content={TrainLCDIMG} />
+      </Helmet>
       <TrainLCDFirstSection />
       <TrainLCDConceptSection />
       <TrainLCDDesignSection />
