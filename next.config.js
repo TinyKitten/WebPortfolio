ff17e2c3c54c5746bc3b85ad29102f0b59b521a8
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-10-28 15:22:48 +0900

    experimental false

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  experimental: {
-    concurrentFeatures: true,
-    serverComponents: true,
-  },
+  // experimental: {
+  //   concurrentFeatures: true,
+  //   serverComponents: true,
+  // },
   images: {
     formats: ['image/avif', 'image/webp'],
   },
