4e9ec0ef1b966439040c7ac677b8fbb4195d98fd
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-03-22 14:24:45 +0900

    アホ

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 module.exports = withBundleAnalyzer({
-  experimental: {
-    concurrentFeatures: true,
-  },
   images: {
     formats: ['image/avif', 'image/webp'],
   },
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,6 +24,16 @@ function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
       <Head>
         <title>TinyKitten</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Header />
       <Component {...pageProps} />
