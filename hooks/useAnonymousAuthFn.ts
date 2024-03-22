4d3772e2a3054cfcdb0f5b1b81e6f52e4bbdb70c
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-03-22 09:44:57 +0000

    そもそも状態管理が不要そう

--- hooks/useAnonymousAuthFn.ts
+++ hooks/useAnonymousAuthFn.ts
@@ -1,17 +1,14 @@
 import { useCallback } from 'react';
-import { useAnonymousUserStore } from './useAnonymousUserStore';
 
 export const useAnonymousAuthFn = (): (() => Promise<void>) => {
-  const setUser = useAnonymousUserStore((state) => state.setUser);
   const updateUserState = useCallback(async () => {
     const { initializeAuth, signInAnonymously } = await import('firebase/auth');
 
     const { firebaseApp } = await import('../lib/firebase');
     const auth = initializeAuth(firebaseApp, {});
 
-    const credential = await signInAnonymously(auth);
-    setUser(credential.user);
-  }, [setUser]);
+    await signInAnonymously(auth);
+  }, []);
 
   return updateUserState;
 };
